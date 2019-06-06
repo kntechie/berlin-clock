01dcc285d9c6e7bd1f076d7ab8cc3b932f9da2d3
kumaran <kumaran26kumaran@gmail.com>
2019-06-07 01:52:08 +0200

    refactor: Grouping of test cases and adding common code to before each

--- src/BerlinClock.test.js
+++ src/BerlinClock.test.js
@@ -9,24 +9,32 @@ configure({ adapter: new Adapter() });
 
 describe('Berlin clock', function(){
 
-  it('at 00:00:00 should have all the lamp to be OFF (OOOO)', () => {
-    var wrapper = shallow(<BerlinClock />);
-    var clock = wrapper.instance();
-    var time = new Date(2019, 6, 7, 0, 0, 0);
+  var wrapper;
+  var clock;
+  
+  describe('Single minutes row', function(){
 
-    clock.setTime(time);
+      beforeEach(function(){
+          wrapper = shallow(<BerlinClock />);
+          clock = wrapper.instance();
+      });
 
-    expect(clock.bottomMinutes()).toBe('OOOO');
-  });
+      it('at 00:00:00 should have all the lamp to be OFF (OOOO)', () => {
+          var time = new Date(2019, 6, 7, 0, 0, 0);
+      
+          clock.setTime(time);
+      
+          expect(clock.bottomMinutes()).toBe('OOOO');
+      });
 
-  it('at 23:59:59 should have all the lamp to be ON (YYYY)', () => {
-    var wrapper = shallow(<BerlinClock />);
-    var clock = wrapper.instance();
-    var time = new Date(2019, 6, 7, 23, 59, 59);
+      it('at 23:59:59 should have all the lamp to be ON (YYYY)', () => {
+          var time = new Date(2019, 6, 7, 23, 59, 59);
+      
+          clock.setTime(time);
+      
+          expect(clock.bottomMinutes()).toBe('YYYY');
+      });
 
-    clock.setTime(time);
-
-    expect(clock.bottomMinutes()).toBe('YYYY');
   });
 
 });
